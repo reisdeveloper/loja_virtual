35bc1406a99e3d7449dcd7e35909b56c74dea482
Jhonatan Reis <reisdeveloper10@gmail.com>
2024-03-13 02:00:00 -0300

    initial

---
+++ android/app/src/main/kotlin/com/example/loja_demo_virtual/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.loja_demo_virtual
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/config/theme/theme.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appTheme = ThemeData(primaryColor: const Color(0xFF070F2B));
